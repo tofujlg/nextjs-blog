f011ce2a450e8a9063384f726c64d54d70d8758b
奥村　耕平 <tofuredbull@gmail.com>
2023-09-24 07:47:22 +0900

    Installed TS and CSS stylings

---
+++ pages/posts/first-post.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import Head from "next/head";
+
+export default function FirstPost() {
+  return (
+    <>
+      <Head>
+        <title>First Post</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          Back to home
+        </Link>
+      </h2>
+    </>
+  );
+}
